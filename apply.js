42dd84f927b1531915c176a36ae321fa71cc4110
Yungeng Xu <xuyg@rd.netease.com>
2015-08-18 16:22:45 +0800

    add php post

---
+++ apply.js
@@ -0,0 +1,29 @@
+(function(){
+	$(".select-control").change(function () {
+		var select = $(this).children('option:selected').val();
+		if (select == 'film') {
+			$('.apply_film').css('display', 'block');
+			$('.communication').css('display', 'none');
+			$('.design').css('display', 'none');
+			$('.innovation').css('display', 'none');
+		};
+		if (select == 'communication') {
+			$('.apply_film').css('display', 'none');
+			$('.communication').css('display', 'block');
+			$('.design').css('display', 'none');
+			$('.innovation').css('display', 'none');
+		};
+		if (select == 'design') {
+			$('.apply_film').css('display', 'none');
+			$('.communication').css('display', 'none');
+			$('.design').css('display', 'block');
+			$('.innovation').css('display', 'none');
+		};
+		if (select == 'innovation') {
+			$('.apply_film').css('display', 'none');
+			$('.communication').css('display', 'none');
+			$('.design').css('display', 'none');
+			$('.innovation').css('display', 'block');
+		};
+	})
+})()
